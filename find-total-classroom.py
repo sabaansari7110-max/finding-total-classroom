e752a66256d1a1b7ec58fde98acf4f9bba964e46
Saba Ansari <sabaansari7110@gmail.com>
2026-01-30 17:14:51 +0530

    Add files via upload

---
+++ find-total-classroom.py
@@ -0,0 +1,5 @@
+#"python","java", "c++", "python", "java script", "java", "python", "java", "c++", "c"
+
+classroom= {"python","java", "c++", "python", "java script", "java", "python", "java", "c++", "c"}
+
+print("total numbers of classrooms:", len(classroom))
